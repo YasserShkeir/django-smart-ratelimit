91acca5a19ee21b39b87e35dfc96a54e1c46c163
Yasser Shkeir <shkeiryasser@gmail.com>
2025-07-15 18:01:40 +0300

    chore: update version to 0.7.1 in CHANGELOG and relevant files

--- examples/integrations/drf_integration/__init__.py
+++ examples/integrations/drf_integration/__init__.py
@@ -19,7 +19,7 @@ Usage:
     router.register(r'posts', PostViewSet)
 """
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 __author__ = "Django Smart Ratelimit Team"
 
 # Import key classes for convenience
