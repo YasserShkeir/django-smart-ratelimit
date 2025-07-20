d312426b7e7149940f4b47750f3d77e2393198f7
Yasser Shkeir <shkeiryasser@gmail.com>
2025-07-20 14:30:06 +0300

    bump: version 0.7.5

--- django_smart_ratelimit/__init__.py
+++ django_smart_ratelimit/__init__.py
@@ -5,7 +5,7 @@ with support for multiple backends, algorithms (including token bucket),
 and comprehensive rate limiting strategies.
 """
 
-__version__ = "0.7.4"
+__version__ = "0.7.5"
 __author__ = "Yasser Shkeir"
 
 # Algorithms
