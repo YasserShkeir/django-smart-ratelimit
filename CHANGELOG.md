6145e1c81a275167118dc33d5e87ad578a349de4
Yasser Shkeir <shkeiryasser@gmail.com>
2025-07-20 14:29:12 +0300

    bump: version 0.7.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.7.4] - 2025-07-20
+
 ## [0.7.3] - 2025-07-20
 
 ### Added
--- django_smart_ratelimit/__init__.py
+++ django_smart_ratelimit/__init__.py
@@ -5,7 +5,7 @@ with support for multiple backends, algorithms (including token bucket),
 and comprehensive rate limiting strategies.
 """
 
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 __author__ = "Yasser Shkeir"
 
 # Algorithms
