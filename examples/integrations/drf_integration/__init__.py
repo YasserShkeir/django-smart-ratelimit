442d4ac196a0dcac3aef6bc41a09ebf5a9a354d9
Yasser Shkeir <shkeiryasser@gmail.com>
2025-10-06 13:25:17 +0330

    bump: version 0.8.9

--- examples/integrations/drf_integration/__init__.py
+++ examples/integrations/drf_integration/__init__.py
@@ -19,7 +19,7 @@ Usage:
     router.register(r'posts', PostViewSet)
 """
 
-__version__ = "0.8.8"
+__version__ = "0.8.9"
 __author__ = "Django Smart Ratelimit Team"
 
 # Import key classes for convenience
